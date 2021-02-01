e13b1003e39626b8b9ded4a8897c6c71421b1a30
Vlad Petrushkevich <vladpetrushkevich1999@gmail.com>
2021-02-01 10:21:06 +0300

    feat: add bottom navigation view

--- app/src/main/java/com/vladuken/eventful/MainActivity.kt
+++ app/src/main/java/com/vladuken/eventful/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.vladuken.eventful
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vladuken.features.events.presentation.events.favorite.FavoriteEventListFragment
 import com.vladuken.features.events.presentation.events.recent.RecentEventListFragment
 
@@ -12,8 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bnw = findViewById<BottomNavigationView>(R.id.bnwMain)
+
         if (savedInstanceState == null) {
-            changeFragment(FavoriteEventListFragment())
+            bnw.selectedItemId = R.id.menu_recent
+            changeFragment(RecentEventListFragment())
+        }
+
+        bnw.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_recent -> {
+                    changeFragment(RecentEventListFragment())
+                    true
+                }
+                R.id.menu_favorite -> {
+                    changeFragment(FavoriteEventListFragment())
+                    true
+                }
+                else -> false
+            }
         }
 
     }
